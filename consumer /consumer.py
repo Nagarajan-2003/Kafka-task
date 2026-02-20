cefc5cd44a85dceb2f4cbc648cd63fe955081d1f
Nagarajan M <nagarajanmnagarajan77@gmail.com>
2026-02-20 10:51:48 +0530

    Create consumer.py

---
+++ consumer /consumer.py
@@ -0,0 +1,15 @@
+from kafka import KafkaConsumer
+import json
+
+consumer = KafkaConsumer(
+    "orders",
+    bootstrap_servers='localhost:9092',
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='order-group',
+    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+)
+
+for message in consumer:
+    order = message.value
+    print("Consumed:", order)
